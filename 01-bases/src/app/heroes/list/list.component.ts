67070bdd97ba46938a4e8bf176ee226f258c925a
Hector Camacho <carabezmartin@outlook.com>
2023-05-23 22:22:29 -0600

    hcamacho: se trabajo con el ngif en el componente de hero y con ngfor en el componente de list

--- 01-bases/src/app/heroes/list/list.component.ts
+++ 01-bases/src/app/heroes/list/list.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class ListComponent {
 
+  public heroNames:string [] = ['SpiderMan','IronMan','Thor','Hulk','Capitan America','Black Panther','Hawk Eye','Black Widow','AntMan']
+
 }
