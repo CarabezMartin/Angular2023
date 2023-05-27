414f4bb0ed7378658ed2037e184dd5ad6de7b41a
Hector Camacho <carabezmartin@outlook.com>
2023-05-26 21:32:34 -0600

    hcamacho: se genera el modulo de heroes para separar los componentes heroe component y list component

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -2,19 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent,    
-    HeroComponent,
-    ListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    CounterModule
+    BrowserModule,   
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
