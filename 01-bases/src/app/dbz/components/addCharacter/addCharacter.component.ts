4f8fd4b0ee5d8d475a8ee9b2c934f9627c3e6f3b
Hector <hector.camacho@iidea.com.mx>
2023-05-29 16:45:02 -0600

    hcamacho: se crean los componentes list y addCharacter para el modulo de Bbz

---
+++ 01-bases/src/app/dbz/components/addCharacter/addCharacter.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-addCharacter',
+  templateUrl: './addCharacter.component.html',
+  styleUrls: ['./addCharacter.component.css']
+})
+export class AddCharacterComponent {
+
+  constructor() { }
+
+}
--- 01-bases/src/app/dbz/dbz.module.ts
+++ 01-bases/src/app/dbz/dbz.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListDbzComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/addCharacter/addCharacter.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListDbzComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule
