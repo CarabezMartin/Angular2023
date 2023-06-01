ab462a413ccd21d0a41a19b837057f36e91df413
Hector Camacho <carabezmartin@outlook.com>
2023-05-31 22:44:11 -0600

    hcamacho: se aplica el concepto de ngModel para enlasar textbox HTML a variables de typescript

--- 01-bases/src/app/dbz/components/addCharacter/addCharacter.component.ts
+++ 01-bases/src/app/dbz/components/addCharacter/addCharacter.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
 
 @Component({
   selector: 'app-dbz-addCharacter',
   templateUrl: './addCharacter.component.html',
   styleUrls: ['./addCharacter.component.css']
 })
+
 export class AddCharacterComponent {
 
   constructor() { }
+ 
+  public character:Character = {name:'',power:0};
+
+  addCharacter()
+  {
+    console.log(this.character)
+  }
 
 }
