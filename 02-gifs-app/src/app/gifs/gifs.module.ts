c211db2c2e2865804ad6d294101ab58d44391efb
Hector Camacho <carabezmartin@outlook.com>
2023-06-13 22:25:48 -0600

    creacion de componentes de search(para la barra de buscar) y card-list(para generar la lista de gifs)

--- 02-gifs-app/src/app/gifs/gifs.module.ts
+++ 02-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    SearchComponent,
+    CardListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:
+  [
+    HomeComponent
   ]
 })
 export class GifsModule { }
