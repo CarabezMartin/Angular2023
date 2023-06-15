e0f8bc14ee56020fd99f178b677e2aea0f606db7
Hector Camacho <carabezmartin@outlook.com>
2023-06-14 19:23:10 -0600

    hcamacho: concepto de @ViewChild y creacion y aplicacion de servicios

--- 02-gifs-app/src/app/gifs/components/search/search.component.ts
+++ 02-gifs-app/src/app/gifs/components/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+
+  constructor(private gifsService:GifsService){}
+
+  @ViewChild('txtBuscar')
+  tagInput !: ElementRef<HTMLInputElement>;
+
+  searchTag()
+  {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log(newTag);
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+  }
 
 }
