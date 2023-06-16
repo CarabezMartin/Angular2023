5a9703ff12ac6ba06712e6ee6991238e050e9dcb
Hector Camacho <carabezmartin@outlook.com>
2023-06-15 22:32:25 -0600

    hcamacho: nos conectamos a la api para consultar los gifs y nos traemos la informacion de la api. Se ve concepto de peticiones http

--- 02-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 02-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,19 +6,43 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   private _tagsHistory:string[] = [];
+  private apiKey : string = '0nS3czmnCBFFTwe7F1PegcywiVasRucE';
+  private urlApi : string = 'https://api.giphy.com/v1/gifs';
 
   get tagsHistory()
   {
     return [...this._tagsHistory];
   }
 
+  private organizeHistory(tag:string)
+  {
+    tag = tag.toLocaleLowerCase();
+
+    if(this._tagsHistory.includes(tag))
+    {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag != tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.slice(0,10);
+  }
+
   searchTag(newTag:string)
   {
-    this._tagsHistory.unshift(newTag);
+    if(newTag.length == 0)return;
 
-    console.log('Valores buscados: ' + this._tagsHistory);
+    this.organizeHistory(newTag);
+
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=0nS3czmnCBFFTwe7F1PegcywiVasRucE&q=dragon ball&limit=10')
+    //   .then(resp => resp.json()).then(data => console.log(data))
+    
+    this.http.get(this.urlApi + '/search?api_key='+ this.apiKey +'&q=' + newTag + '&limit=10')
+    .subscribe(resp => {
+      console.log(resp);
+    })
+
   }
 }
