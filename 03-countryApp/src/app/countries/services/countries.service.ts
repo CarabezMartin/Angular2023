b82d5c5174d383d67262f8fbe38b9704b21b3dfc
Hector Camacho <carabezmartin@outlook.com>
2023-07-12 19:03:55 -0600

    hcamacho: optimizacion del servicio countries.service.ts

--- 03-countryApp/src/app/countries/services/countries.service.ts
+++ 03-countryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country, Maps } from '../interfaces/country.interface';
 
 @Injectable({providedIn: 'root'})
@@ -11,6 +11,16 @@ export class CountriesService {
     private apiUrl:string = 'https://restcountries.com/v3.1/';
 
     constructor(private httpClient: HttpClient) { }
+
+    private getCountryRequest(url:string): Observable<Country[]>
+    {
+        return this.httpClient.get<Country[]>(url)
+            .pipe(
+            catchError(error => of([])),
+            delay(2000)
+        );
+
+    }
 
     searchCountryByAlphaCode(code:string): Observable<Country | null>
     {
@@ -23,25 +33,19 @@ export class CountriesService {
     
     searchCapital(termino:string) : Observable<Country[]>
     {
-        return this.httpClient.get<Country[]>(this.apiUrl+'capital/'+termino)
-            .pipe(
-                catchError(error => of([]))
-            );
+        const url = this.apiUrl+'capital/'+termino;
+        return this.getCountryRequest(url);
     }
     
     searchCountry(termino:string) : Observable<Country[]>
     {
-        return this.httpClient.get<Country[]>(this.apiUrl+'name/'+termino)
-            .pipe(
-                catchError(error => of([]))
-            );
+        const url = this.apiUrl+'name/'+termino
+        return this.getCountryRequest(url);
     }
     
     searchRegion(termino:string) : Observable<Country[]>
     {
-        return this.httpClient.get<Country[]>(this.apiUrl+'region/'+termino)
-            .pipe(
-                catchError(error => of([]))
-            );
+        const url = this.apiUrl+'region/'+termino
+        return this.getCountryRequest(url);
     }
 }
