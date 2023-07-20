d5d59c76e2caa764e2f1c3d19745d5fd61ccc064
Hector Camacho <carabezmartin@outlook.com>
2023-07-19 22:24:55 -0600

    hcamacho: se aplica el metodo OnDestroy para cerrar la suscripcion ya que todas las suscripciones deben ser cerradas excepto las peticiones http(get,post,delete,etc)

--- 03-countryApp/src/app/shared/components/search-box/search-box.component.ts
+++ 03-countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -1,14 +1,15 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.css']
 })
-export class SearchBoxComponent implements  OnInit{
-  
+export class SearchBoxComponent implements  OnInit, OnDestroy{
+   
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?:Subscription;
 
   @Input() public placeholder = '';
 
@@ -21,15 +22,21 @@ export class SearchBoxComponent implements  OnInit{
   @Output() onDebounce = new EventEmitter<string>;
 
   ngOnInit(): void {
-    this.debouncer
+
+    this.debouncerSuscription = this.debouncer
     .pipe
     (
       debounceTime(2000)
     )
     .subscribe
     (
-      value => this.onDebounce.emit(value)
+      value => this.onDebounce.emit(value)    
     );
+  }
+
+  //Se manda a llamar cuando el componente se destruye
+  ngOnDestroy(): void {
+    this.debouncerSuscription?.unsubscribe();
   }
 
   emitValue(value:string, typeSearch:string)
